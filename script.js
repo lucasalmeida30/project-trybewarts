6b10c7391be246ed2133edb773f0d360a50d1a0c
lucasalmeida30 <lucas_almeida12@icloud.com>
2022-09-15 09:10:38 -0300

    finalizado erros linter

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
 const inputEmail = document.getElementById('login');
 const inputSenha = document.getElementById('login-senha');
 const button = document.getElementById('button');
-let emailTrybe = 'tryber@teste.com';
-let senhaTrybe = '123456';
+const emailTrybe = 'tryber@teste.com';
+const senhaTrybe = '123456';
 
-button.addEventListener('click', validarFormulario);
-
-
-function validarFormulario (){
-    let email = inputEmail.value;
-    let senha = inputSenha.value;
-
-      if(email === emailTrybe && senha === senhaTrybe){
-        alert('Olá, Tryber!');
-        
-    } else {
-        alert('Email ou senha inválidos.');
-    }
+function validarFormulario() {
+  const email = inputEmail.value;
+  const senha = inputSenha.value;
+  if (email === emailTrybe && senha === senhaTrybe) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 }
-
-
-
-// Requisito 18 -------- Função para habilitar o button 
-
+button.addEventListener('click', validarFormulario);
 const checkboxInfos = document.querySelector('#agreement');
 const buttonSubmit = document.querySelector('#submit-btn');
-
-checkboxInfos.addEventListener('change', () => { //O changeevento é disparado para os elementos <input>, <select>e <textarea>quando o usuário modifica o valor do elemento. https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event
-    buttonSubmit.disabled = !checkboxInfos.checked;
+checkboxInfos.addEventListener('change', () => {
+  buttonSubmit.disabled = !checkboxInfos.checked;
 });
-
-
